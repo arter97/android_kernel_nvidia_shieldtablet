dab4532dbe694a7075dd18256a738c8d1f7e2dbb
Vikram Mulukutla <markivx@codeaurora.org>
2014-10-17 15:40:42 -0700

    idle: exit the cpu_idle_poll loop if cpu_idle_force_poll is cleared
    cpu_idle_poll_ctrl allows the enabling/disabling of the idle
    polling mode; this mode allows a CPU to spin waiting for a
    new task to be scheduled rather than having to execute the
    arch specific idle code.
    
    However, the loop that checks for a new task does not look
    at the flag that enables idle polling mode. So, the CPU may
    continue to spin even though the aforementioned flag has
    been cleared. Since the CPU is already in idle, it may be
    a while before a task is scheduled, precluding potential
    power savings.
    
    Modify the while loop conditional in question to also check
    if the cpu_idle_force_poll flag is set.
    
    Change-Id: Ia2e83af97890dc399b86e090459a41d31ce28b6c
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- kernel/cpu/idle.c
+++ kernel/cpu/idle.c
@@ -54,7 +54,7 @@ static inline int cpu_idle_poll(void)
 	 * test_ti_thread_flag_relaxed(), as tif_need_resched_relaxed()
 	 * ultimately invokes test_ti_thread_flag_relaxed().
 	 */
-	while (!tif_need_resched_relaxed())
+	while (!tif_need_resched_relaxed() && cpu_idle_force_poll)
 		cpu_read_relax();
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, smp_processor_id());
 	rcu_idle_exit();
