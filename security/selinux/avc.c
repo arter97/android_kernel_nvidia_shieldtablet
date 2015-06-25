934389ea2db2efda6fadf8efebaeb6b29c562a48
arter97 <qkrwngud825@gmail.com>
2015-06-26 01:46:22 +0900

    selinux: loose everything
     - SELinux is now force-set to permissive
     - Every access is returned with success
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -997,12 +997,6 @@ static noinline int avc_denied(u32 ssid, u32 tsid,
 				u16 cmd, unsigned flags,
 				struct av_decision *avd)
 {
-	if (flags & AVC_STRICT)
-		return -EACCES;
-
-	if (selinux_enforcing && !(avd->flags & AVD_FLAGS_PERMISSIVE))
-		return -EACCES;
-
 	avc_update_node(AVC_CALLBACK_GRANT, requested, cmd, ssid,
 				tsid, tclass, avd->seqno, NULL, flags);
 	return 0;
